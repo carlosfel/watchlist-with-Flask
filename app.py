7d26aa2410f7e8a351d839f9c853f3cbf96225a3
carlosfel <carlosfelhunter@gmail.com>
2019-07-29 18:01:54 +0800

    minimal2

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import sys,os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # __file__获取执行文件相对路径，整行为取上一级的上一级目录
+sys.path.append(BASE_DIR)
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Welcome to My Watchlist!'
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % name
